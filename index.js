f4753ee8badbe16208be7dc1e06940d439bc03e4
axuebin <xbmail2013@163.com>
2017-08-18 21:43:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function isInSight(el) {
+  const bound = el.getBoundingClientRect();
+  const clientHeight = window.innerHeight;
+  //如果只考虑向下滚动加载
+  //const clientWidth=window.innerWeight;
+  return bound.top <= clientHeight + 100;
+}
+
+function checkImgs() {
+  const imgs = document.querySelectorAll('.my-photo');
+  Array.from(imgs).forEach(el => {
+    if (isInSight(el)) {
+      loadImg(el);
+    }
+  })
+}
+
+function loadImg(el) {
+  if (!el.src) {
+    const source = el.getAttribute("data-src");
+    el.src = source;
+  }
+}
+function throttle(fn, mustRun = 500) {
+  const timer = null;
+  let previous = null;
+  return function() {
+    const now = new Date();
+    const context = this,
+    const args = arguments;
+    if (!previous)
+      previous = now;
+    const remaining = now - previous;
+    if (mustRun && remaining >= mustRun) {
+      fn.apply(context, args);
+      previous = now;
+    }
+  }
+}
